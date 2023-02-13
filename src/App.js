4698b234a1fd7dd9f4d2fc11a961eb18b16f88f6
Xahri <bassemalmuttar@gmail.com>
2023-02-13 20:22:17 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Stars, Stats, Environment, Center } from "@react-three/drei";
+import { PCFSoftShadowMap, sRGBEncoding } from "three";
+import { useState, Suspense } from "react";
 
-function App() {
+import { Overlay } from './components/Overlay';
+import TextGenerator from './components/TextGenerator';
+
+export default function App() {
+  const [inputText, setInputText] = useState("Hello");
+  const maxTextLength = 12;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <Canvas dpr={[1, 2]} shadows gl={{ preserveDrawingBuffer: true, antialias: true, physicallyCorrectLights: true, toneMappingExposure: 0.5, shadowMap: { enabled: true, type: PCFSoftShadowMap }, outputEncoding: sRGBEncoding }} camera={{ fov: 45, position: [0, 0, 36]}} orthographic={false} >
 
-export default App;
+        <OrbitControls autoRotate={false} autoRotateSpeed={0.4} enablePan={true}/>
+        <Suspense fallback={null}>
+          <ambientLight />
+          <Center >
+            <TextGenerator text={inputText} />
+          </Center>
+          <Stars radius={400} depth={50} count={1000} factor={8} />
+          <Stats />
+          <Environment preset="dawn" background blur={0.6} />
+        </Suspense>
+
+      </Canvas>
+      
+      <Overlay />
+      <div className="text-field">
+        <input
+          type="text"
+          value={inputText}
+          onChange={e => setInputText(e.target.value)}
+          maxLength={maxTextLength}
+        />
+        <span> {maxTextLength - inputText.length} </span>
+      </div>
+    </>
+  )
+}
---
+++ src/components/Overlay.jsx
@@ -0,0 +1,13 @@
+export function Overlay() {
+  
+    return (
+        <div style={{ pointerEvents: 'none', position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', fontFamily: 'Bebas Neue', fontSize: '1em', fontWeight: '24', opacity: '100%', letterSpacing: '0.04em' }}>
+            <a href="https://github.com/Xahri" target="_blank" rel="noopener noreferrer" style={{ position: 'absolute', bottom: 40, left: 90, fontSize: '13px' }}>
+                Bassem Youssef —
+            </a>
+            <div style={{ position: 'absolute', bottom: 40, right: 90, fontSize: '13px', color: 'white' }}>
+                13/02/2023
+            </div>
+        </div>
+    )
+  }
